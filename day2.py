a1433078159ff085f60518531bf0d32d825f369d
root <root@station208.example.com>
2018-08-17 17:19:45 +0530

    first python code

---
+++ day2.py
@@ -0,0 +1,31 @@
+
+import  time
+
+print  "Hello world "
+x=10
+y=20
+
+time.sleep(2)
+print  x+y
+
+#  taking input from  user in python 2
+
+n1=raw_input("type any number :   ")
+
+n2=raw_input("type any number :   ")
+
+z=int(n1)+int(n2)
+time.sleep(2)
+print  "sum of given two numbers is  :   ",z
+
+'''
+take 5 input from user and make a tuple with only uniq elements 
+
+'''
+
+
+
+
+
+
+
